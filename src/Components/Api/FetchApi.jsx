e3d5426750df943649ecf13acf24fbe124d30663
AnishaBhattacharjee <anishab163@gmail.com>
2023-06-26 14:55:01 +0530

    home page about section changes

---
+++ src/Components/Api/FetchApi.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BaseUrl="http://127.0.0.1:3002"
+
+export const getAbout=async()=>{
+    try {
+        return await axios.get(`${BaseUrl}/about`)
+    }
+    catch (error) {
+        console.log("Error while fetching data from the Api", error.message);
+    }
+}
